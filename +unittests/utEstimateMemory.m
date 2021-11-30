4a67ffb49603c3bf9f06afa27ec0b83cd3f1637d
Angelo D'Amante <angelo.damante16@gmail.com>
2021-11-30 19:18:31 +0100

    Update unittests dir

--- +unittests/utEstimateMemory.m
+++ +unittests/utEstimateMemory.m
@@ -2,19 +2,29 @@ function utEstimateMemory()
 arduinoUno      = 32768;    % Byte
 arduinoMega     = 262144;   % Byte
 
+% Dataset
 dhFile          = 'data/dBraccioDhParam.mat'; 
 braccioFile     = 'data/dBraccioRobot5Dof.mat';
+propsFile       = 'data/dBraccioProperties.mat';
+pointsFile      = 'data/dPointsTest.mat';
+
+% Usage memory
 dhData          = functions.evaluateUsageMemory(dhFile);
 braccioData     = functions.evaluateUsageMemory(braccioFile);
+propsData       = functions.evaluateUsageMemory(propsFile);
+pointsData      = functions.evaluateUsageMemory(pointsFile);
+
+% Data Tot
+dDataEstimation = dhData + braccioData + propsData + pointsData;
 
 %%%% Estimation data
-if(dhData + braccioData + 20000 < arduinoUno)
+if(dDataEstimation + 20000 < arduinoUno)
     disp('Ut: PASS ---- Probably good for arduino Uno');
 else
     disp('Ut: FAIL ---- Probably bad for arduino Uno');
 end
 
-if(dhData + braccioData + 20000 < arduinoMega)
+if(dDataEstimation + 20000 < arduinoMega)
     disp('Ut: PASS ---- Probably good for arduino Mega');
 else
     disp('Ut: FAIL ---- Probably bad for arduino Mega');
--- +unittests/utFkIk.m
+++ +unittests/utFkIk.m
@@ -1,6 +1,14 @@
 function utFkIk()
 
-dh = load('data/dBraccioDhParam.mat');
+% DH parameters
+adA             = [0; 125; 125; 195; 0];
+adAlpha         = [pi/2; 0; 0; 0; pi/2];
+adD             = [70; 0; 0; 0; 0];
+adOffset        = [-pi/2; 0; -pi/2; -pi/2; 0];
+
+% Braccio properties
+adHome5rad      = [pi/2; pi/2; pi/2; pi/2; 0];
+adWS            = [400; 120; 220];
 
 dt0 = tic; % init cpu timer
 
@@ -15,7 +23,7 @@ oJointWrist     = classes.Joint(deg2rad(0)-pi/2, deg2rad(180)-pi/2);
 oLinkGround     = classes.Link(70);
 oLinkArm        = classes.Link(125);
 oLinkForearm    = classes.Link(125);
-oLinkHand       = classes.Link(185);
+oLinkHand       = classes.Link(195);
 
 % Robot definition
 aoJoints        = [ oJointBase; 
@@ -33,38 +41,37 @@ oBraccio        = classes.Robot5Dof(aoJoints(1), aoJoints(2),   ...
                                     aoJoints(3), aoJoints(4),   ... 
                                     aoJoints(5), aoLinks(1),    ...
                                     aoLinks(2), aoLinks(3),     ...
-                                    aoLinks(4));
+                                    aoLinks(4), adHome5rad);
 dtf = toc(dt0); % stopwatch timer
 
 fprintf('UT: PASS ---- Robot built in: %2.2f ms\n', dtf*1000);
 
 % UT: Inner WS points generation
-adWS        = [400; 120; 220];
-dRadius     = adWS(1);
-dIr         = adWS(2);
-dIh         = adWS(3);
+dRadius         = adWS(1);
+dIr             = adWS(2);
+dIh             = adWS(3);
 
-dataset     = functions.makeWS(dRadius, dIr, dIh);
+dataset         = functions.makeWS(dRadius, dIr, dIh);
 
-bTest       = true;
+bTest           = true;
 for p=1:size(dataset,2)
-    point   = dataset(:, p);
+    point       = dataset(:, p);
     
     % IK
-    dt0IK   = tic;
-    [bF, q] = oBraccio.IKIN(point);
-    dtfIK   = toc(dt0IK);
+    dt0IK       = tic;
+    [bF, q]     = oBraccio.IKIN(point);
+    dtfIK       = toc(dt0IK);
     
     % Mapper
-    Q       = [q+pi/2; 0];
+    Q           = [q+pi/2; 0];
     
     % FK
-    dt0FK   = tic;
-    adT     = oBraccio.KIN(dh.adD, Q, dh.adA, dh.adAlpha, dh.adOffset);
-    dtfFK   = toc(dt0FK);
+    dt0FK       = tic;
+    adT         = oBraccio.KIN(adD, Q, adA, adAlpha, adOffset);
+    dtfFK       = toc(dt0FK);
     
     % Tracker test
-    bTest   = bTest && bF && (abs(norm(adT(1:3,end) - point)) < 1e-04);
+    bTest       = bTest && bF && (abs(norm(adT(1:3,end) - point)) < 1e-04);
 end
 
 % Timing
@@ -73,14 +80,14 @@ fprintf('UT: PASS ---- FK computed in: %2.2f ms\n', dtfFK*1000);
 
 % Violation Test
 [bFv, qv]   = oBraccio.IKIN([500; 0; 0]);
-if (qv == zeros(4,1))
-    bFv = ~bFv && true;
+if (qv == adHome5rad(1:4))
+    bFv         = ~bFv && true;
 else
-    bFv = ~bFv && false;
+    bFv         = ~bFv && false;
 end
 
 % Check
-bTest = bTest && bFv;
+bTest           = bTest && bFv;
 if(bTest == true)
     disp('Ut: PASS ---- FK_IK class works fine');
 else
